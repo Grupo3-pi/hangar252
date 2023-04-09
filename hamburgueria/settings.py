03002a001f2b6d53f3f5524c6335fb90971639ce
Caio-CGodoy <2109745@aluno.univesp.br>
2023-04-09 12:41:53 -0300

    deploy

--- hamburgueria/settings.py
+++ hamburgueria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*9v!y#rvq&&tu(ihbhi85ev*01wbtsq1ii_oa=7x%t(0_awp-!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hangar252.vercel.app']
+ALLOWED_HOSTS = [' * ']
 ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
