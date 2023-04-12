3c52277905cb8244c40add2b3716688934f38ae5
Caio-CGodoy <2109745@aluno.univesp.br>
2023-04-11 23:48:34 -0300

    deploy

--- cardapio/models.py
+++ cardapio/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Ingredientes(models.Model):
+    nome = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.nome
+
+class Prato(models.Model):
+    nome_prato = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.nome_prato
+
+class Cardapio(models.Model):
+    prato = models.ForeignKey(Prato, on_delete=models.DO_NOTHING)
+    Ingrediente = models.ForeignKey(Ingredientes, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return self.prato
+
--- hamburgueria/settings.py
+++ hamburgueria/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'hamburgueria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cardapio_db',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
